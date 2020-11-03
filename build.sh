dfa02c046d709a087961234c047e89fd0f590f4c
Mark Eissler <mark@bunker5.com>
2020-11-03 14:12:14 -0800

    Update build scripts.

--- build.sh
+++ build.sh
@@ -1,10 +1,9 @@
-#!/usr/bin/env bash
-
-set -eou pipefail
+#!/bin/bash
 
 rm -rf build/kubano
 GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go build -o build/kubano
 
-docker build -t danielgormly/drone-plugin-kube:0.2.0 -t danielgormly/drone-plugin-kube:latest build
-docker push danielgormly/drone-plugin-kube:0.2.0
-docker push danielgormly/drone-plugin-kube:latest
+docker build -t markeissler/drone-kubulous:0.2.0 -t markeissler/drone-kubulous:latest build
+docker push markeissler/drone-kubulous:0.2.0
+docker push markeissler/drone-kubulous:latest
+
